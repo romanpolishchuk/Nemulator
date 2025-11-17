b437daa61b5b50712eeaef5e68485e30ff26c9db
Roman Polishchuk <roman.polishchuk.work@gmail.com>
2025-11-17 23:02:11 +0000

    Add file reading

--- src/rom_reader.rs
+++ src/rom_reader.rs
@@ -7,6 +7,7 @@ pub struct iNES_header {
 #[allow(non_camel_case_types)]
 pub struct iNES {
     pub header: iNES_header,
+    pub trainer: Vec<u8>,
     pub prg_rom: Vec<u8>,
     pub chr_rom: Vec<u8>,
 }
@@ -18,9 +19,30 @@ pub fn read_file(filename: &str) -> iNES {
         prg_rom_size: file[4],
         chr_rom_size: file[5],
     };
+    let mut pointer = 16;
+    let mut trainer = vec![];
+    if file[6] & 0b0000_0100 == 1 {
+        trainer.copy_from_slice(&file[pointer..pointer + 512]);
+        pointer += 512;
+    }
+
+    let mut prg_rom = vec![0; 16384 * ines_header.prg_rom_size as usize];
+    if ines_header.prg_rom_size > 0 {
+        prg_rom
+            .copy_from_slice(&file[pointer..pointer + 16384 * ines_header.prg_rom_size as usize]);
+        pointer += 16384 * ines_header.prg_rom_size as usize;
+    }
+
+    let mut chr_rom = vec![0; 8192 * ines_header.prg_rom_size as usize];
+    if ines_header.chr_rom_size > 0 {
+        chr_rom.copy_from_slice(&file[pointer..pointer + 8192 * ines_header.chr_rom_size as usize]);
+        pointer += 8192 * ines_header.chr_rom_size as usize;
+    }
+
     let ines = iNES {
-        prg_rom: vec![0; 16384 * ines_header.prg_rom_size as usize],
-        chr_rom: vec![0; 8192 * ines_header.chr_rom_size as usize],
+        prg_rom: prg_rom,
+        chr_rom: chr_rom,
+        trainer: trainer,
         header: ines_header,
     };
 
